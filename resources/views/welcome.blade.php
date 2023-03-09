ac4a2aac1a884f71dd03dd616a7df07aa2fa28c7
Bhagyaak <kavijanb@gmail.com>
2023-03-09 16:38:15 +0530

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html><html><head><link href="https://fonts.googleapis.com/css?family=Inter&display=swap" rel="stylesheet" /></head><body><div class="v1_2"><div class="v2_3"></div><span class="v2_4">Home  about  classes  Teachers  </span><div class="v2_6"></div><div class="v8_2"></div></div></body></html> <br/><br/> <style>* {
+  box-sizing: border-box;
+}
+body {
+  font-size: 14px;
+}
+.v1_2 {
+  width: 559px;
+  height: 596px;
+  background: rgba(255,255,255,1);
+  opacity: 1;
+  position: relative;
+  top: 0px;
+  left: 0px;
+  overflow: hidden;
+}
+.v2_3 {
+  width: 559px;
+  height: 24px;
+  background: rgba(217,217,217,1);
+  opacity: 1;
+  position: relative;
+  top: 0px;
+  left: 0px;
+  overflow: hidden;
+}
+.v2_4 {
+  width: 274px;
+  color: rgba(0,0,0,1);
+  position: absolute;
+  top: 5px;
+  left: 36px;
+  font-family: Inter;
+  font-weight: Regular;
+  font-size: 12px;
+  opacity: 1;
+  text-align: left;
+}
+.v2_6 {
+  width: 174px;
+  height: 173px;
+  background: rgba(217,217,217,1);
+  opacity: 1;
+  position: absolute;
+  top: 42px;
+  left: 278px;
+}
+.v8_2 {
+  width: 250px;
+  height: 161px;
+  background: rgba(217,217,217,1);
+  opacity: 1;
+  position: absolute;
+  top: 122px;
+  left: 36px;
+  overflow: hidden;
+}
+</style>
